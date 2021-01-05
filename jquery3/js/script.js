d13287ea5d10c1d24de48d53578e13ca0b0b84a2
ifte420 <ifte430@gmail.com>
2021-01-05 12:07:36 +0600

    simple slider

---
+++ jquery3/js/script.js
@@ -0,0 +1,12 @@
+//(selector).action();
+$('.banner_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+    speed: 2000,
+    prevArrow: '<i class="fas fa-angle-double-left prv_aar"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right nxt_arr"></i>',
+});
